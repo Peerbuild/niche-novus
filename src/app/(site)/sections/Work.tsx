1017a81949815f54be6d13ff299cf7eaa261c5a1
Priyobroto Kar <priyobrotokar@gmail.com>
2024-10-19 21:32:31 +0530

    Refactor Work section to remove commented out code and adjust styling

--- src/app/(site)/sections/Work.tsx
+++ src/app/(site)/sections/Work.tsx
@@ -4,11 +4,7 @@ import SectionWrapper from "../_components/SectionWrapper";
 
 const Work = () => {
   return (
-    <SectionWrapper className="space-y-16 min-h-screen flex justify-center items-center sticky top-0 md:space-y-40 py-32 bg-background">
-      {/* <SectionTitle
-        title="work"
-        subtitle="he creates custom visuals that elevate live Shows, events, Films, Narratives to an ImmersivE & Otherworldly experience."
-      /> */}
+    <SectionWrapper className="space-y-16 min-h-screen flex justify-center items-center sticky top-0  py-32 bg-background">
       <WorkCarousel />
     </SectionWrapper>
   );
