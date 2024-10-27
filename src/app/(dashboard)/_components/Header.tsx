8bcfabff7a324a463138dd6af447dbb0eda0de3f
Priyobroto Kar <priyobrotokar@gmail.com>
2024-10-27 14:00:40 +0530

    Refactor Header component to adjust padding in the top and bottom

--- src/app/(dashboard)/_components/Header.tsx
+++ src/app/(dashboard)/_components/Header.tsx
@@ -5,7 +5,7 @@ import { getTimeOfDay } from "@/lib/utils";
 
 export const Header = () => {
   return (
-    <header className="text-xl  border-b-2 border-border py-5 flex justify-between items-center">
+    <header className="text-xl  border-b-2 border-border pt-16 pb-8 flex justify-between items-center">
       <div>
         Good {getTimeOfDay()}, <span className="text-accent">Nishad.</span>
       </div>
